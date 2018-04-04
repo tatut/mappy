c5ac4bd226dfa5a283c50bab7995fd9781560846
Tatu Tarvainen <tatu.tarvainen@solita.fi>
2018-04-04 16:51:24 +0300

    Prepare for clojars deploy

--- project.clj
+++ project.clj
@@ -1,10 +1,11 @@
-(defproject mappy "0-SNAPSHOT"
+(defproject mappy "20180404"
   :dependencies [[org.clojure/clojure "1.9.0"]
                  [org.clojure/clojurescript "1.10.126"]
                  [reagent "0.8.0-alpha2"]
                  [figwheel "0.5.15"]]
   :plugins [[lein-cljsbuild "1.1.7"]
             [lein-figwheel "0.5.15"]]
+  :source-paths ["src/cljs"]
   :cljsbuild {:builds
               [{:id "dev"
                 :source-paths ["src/cljs"]
@@ -12,4 +13,5 @@
                 :compiler {:optimizations :none
                            :source-map true
                            :output-to "resources/public/js/mappy.js"
-                           :output-dir "resources/public/js/out"}}]})
+                           :output-dir "resources/public/js/out"}}]}
+  :clean-targets ^{:protect false} ["resources/public/js"])
